91102908914ec0723bb4de0f4abe1d45df85960c
Oliver Lorenz <lorenz@cybus.io>
2016-05-25 21:30:43 +0200

    changed project to use more than standard AFINN-111 list

--- build/index.js
+++ build/index.js
@@ -11,29 +11,41 @@
 var async   = require('async'),
     fs      = require('fs');
 
-/**
- * Read AFINN data from original format
- */
-fs.readFile(__dirname + '/AFINN.txt', function (err, data) {
-    // Storage object
-    var hash = new Object(null);
+var pattern = /(.+)\.txt/i;
+var baseDirectory = __dirname + '/../node_modules/sentiment-lists';
+fs.readdir(baseDirectory, function (err, directoryContent) {
+    directoryContent.forEach(function (item) {
+        var match = pattern.exec(item);
+        if (match != null) {
+            fs.readFile(baseDirectory + '/' + match[0], function (err, data) {
+                // Storage object
+                var hash = new Object(null);
 
-    // Split lines
-    var lines = data.toString().split(/\n/);
-    async.forEach(lines, function (obj, callback) {
-        var item = obj.split(/\t/);
-        hash[item[0]] = Number(item[1]);
-        callback();
-    }, function (err) {
-        if (err) throw new Error(err);
+                // Split lines
+                var lines = data.toString().split(/\n/);
+                async.forEach(lines, function (obj, callback) {
+                    var item = obj.split(/\t/);
+                    hash[item[0]] = Number(item[1]);
+                    callback();
+                }, function (err) {
+                    if (err) throw new Error(err);
 
-        // Write out JSON
-        fs.writeFile(
-            __dirname + '/AFINN.json', 
-            JSON.stringify(hash), 
-        function (err) {
-            if (err) throw new Error(err);
-            process.stdout.write('Complete.');
-        });
+                    // Write out JSON
+                    fs.writeFile(
+                        __dirname + '/' + match[1] + '.json',
+                        JSON.stringify(hash),
+                    function (err) {
+                        if (err) throw new Error(err);
+                        process.stdout.write('Complete.');
+                    });
+                });
+            });
+        }
     });
 });
+
+
+/**
+ * Read AFINN data from original format
+
+*/
--- test/unit/spec.js
+++ test/unit/spec.js
@@ -3,7 +3,7 @@ var sentiment = require('../../lib/index');
 
 test('interface', function (t) {
     t.type(sentiment, 'function', 'module is a function');
-    t.type(sentiment('test'), 'object');
-    t.type(sentiment('test', {test: 10}), 'object');
+    t.type(sentiment('test', 'en_us'), 'object');
+    t.type(sentiment('test', 'en_us', {test: 10}), 'object');
     t.end();
 });
